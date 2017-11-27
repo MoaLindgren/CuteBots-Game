f9ae9abae85e45ad2ec5cec8e018d58f4a427d92
MoaLindgren <moa.lindgren@outlook.com>
2017-11-27 12:22:31 +0100

    Revert "Merge branch 'master' of https://github.com/MoaLindgren/CuteBots-Game"
    This reverts commit 027761d9cff433cd77e96978a140be6ef05e2e02, reversing
    changes made to ce9faec667fe1a88f79b0479acd86f409f1889b4.

--- CuteBot/Assets/Scripts/PlayerManager.cs
+++ CuteBot/Assets/Scripts/PlayerManager.cs
@@ -9,11 +9,6 @@ public class PlayerManager : MonoBehaviour
     bool canDrag = false;
     public bool isDetectable;
 
-   
-
-    SoundManager mcSound;
-
-    private int maxFallDistance = -10;
 
     [SerializeField]
     float climbSpeed;
@@ -25,24 +20,24 @@ public class PlayerManager : MonoBehaviour
     float jumpHeight;
 
     [SerializeField]
-    public float movementSpeed;
+    float movementSpeed;
 
     [SerializeField]
     float pushForce = 2.0f;
 
     GameManager GM;
 
-    GameObject currentStation, currentDragable;
+    GameObject currentStation, currentDragable, station;
 
-    GameObject station;
+    public GameObject model;
 
-   // GameObject manager;
-
-    public CharacterController controller;
+    CharacterController controller;
 
     Vector3 moveDirection = Vector3.zero;
 
     Animator anim;
+
+    bool pulling = false;
 
     public bool IsDetectable
     {
@@ -52,74 +47,18 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-      
         anim = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
-        station = GameObject.Find("Station");
+        station = GameObject.Find("Workbench");
         isDetectable = true;
-      //  manager = GameObject.Find("Manager");
-        mcSound = GameObject.Find("Manager").GetComponent<SoundManager>();
     }
-    /*
-    void RunSound()
-    {
 
-        playerSounds = GetComponent<AudioSource>();
-        while (something)
-        playerSounds.GetComponent<SoundManager>();
-        playerSounds.clip = mcSound.mcSfx[0];
-        playerSounds.Play();
-    }
-    */
     void Update()
     {
-        /*
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        anim.SetFloat("Speed", Mathf.Abs(horizontal));
-        anim.SetFloat("Speed", Mathf.Abs(vertical));
-        */
-
-        //if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-        //{
-        //    //start transition
-        //}
-        if (Input.GetKey(KeyCode.E) && canDrag && currentDragable != null)
-        {
-            float speed = 4.0f;
-            float step = 2f;
-            step = Time.deltaTime * speed;
-            movementSpeed = 4.0f;
-            currentDragable.transform.position = Vector3.MoveTowards(currentDragable.transform.position, transform.position, step); //Får objektet att följa efter spelaren sålänge E hålls in
-        }
-
-        if (Input.GetKey(KeyCode.E) && currentStation != null)
-        {
-            isDetectable = false;
-
-            if (currentStation.gameObject.GetComponent<ItemManager>().StationHealth > 0)
-            {
-                currentStation.GetComponent<ItemManager>().StationHealth -= Time.deltaTime;
-                print(currentStation.GetComponent<ItemManager>().StationHealth);
-
-            }
-            if (currentStation.GetComponent<ItemManager>().StationHealth <= 0)
-            {
-                station.GetComponent<ItemManager>().TriggerCollider.enabled = false;
-                currentStation = null;
-                IsDetectable = true;
-            }
-        }
 
         if (movementSpeed <= 4 && !canDrag)
         {
-            movementSpeed = 6;
-        }
-
-        if (transform.position.y <= maxFallDistance)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            movementSpeed = 5;
         }
 
         // Om vi kan klättra rör vi oss uppåt.
@@ -129,62 +68,88 @@ public class PlayerManager : MonoBehaviour
             {
                 moveDirection = new Vector3(0, Input.GetAxis("Vertical"), 0);
                 moveDirection *= climbSpeed;
-
             }
-
         }
 
         //Rörelsekontroller för spelaren
         if (controller.isGrounded)
         {
+            if (anim.GetBool("isJumping"))
+            {
+                anim.SetBool("isJumping", false);
+            }
+
+            if (Input.GetKey(KeyCode.E) && canDrag && currentDragable != null)
+            {
+                anim.SetBool("isPulling", true);
+                pulling = true;
+                float speed = 4.0f;
+                float step = 2f;
+                step = Time.deltaTime * speed;
+                movementSpeed = 3.0f;
+                currentDragable.transform.position = Vector3.MoveTowards(currentDragable.transform.position, transform.position, step); //Får objektet att följa efter spelaren sålänge E hålls in
+            }
+
+            if (Input.GetKey(KeyCode.Mouse0) && currentStation != null)
+            {
+                isDetectable = false;
+
+                if (currentStation.gameObject.GetComponent<ItemManager>().StationHealth > 0)
+                {
+                    currentStation.GetComponent<ItemManager>().StationHealth -= Time.deltaTime;
+                    print(currentStation.GetComponent<ItemManager>().StationHealth);
+                    anim.SetBool("isHammering", true);
+                }
+                if (currentStation.GetComponent<ItemManager>().StationHealth <= 0)
+                {
+                    station.GetComponent<ItemManager>().TriggerCollider.enabled = false;
+                    currentStation = null;
+                    IsDetectable = true;
+                    anim.SetBool("isHammering", false);
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                anim.SetBool("isPulling", false);
+                pulling = false;
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+                anim.SetBool("isHammering", false);
+
             moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
-            if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
+
+            if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f && !pulling)
             {
                 anim.SetBool("isRunning", true);
             }
-            else
+            else if (!pulling)
             {
                 anim.SetBool("isRunning", false);
             }
             moveDirection = transform.TransformDirection(moveDirection);
-            //runAnim.SetBool("running", true);
+            if (moveDirection != Vector3.zero)
+            {
+                model.transform.rotation = Quaternion.LookRotation(moveDirection);
+            }
             if (Input.GetKey(KeyCode.LeftShift))
-            {                
-                moveDirection *= movementSpeed + 4;
-                
+            {
+                moveDirection *= movementSpeed + 2;
             }
             else
             {
                 moveDirection *= movementSpeed;
             }
-            if (Input.GetButton("Jump"))
+            if (Input.GetButton("Jump") && !pulling)
+            {
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isJumping", true);
+
                 moveDirection.y = jumpHeight;
 
-            
-            if (anim.isRunning == true)
-            {
-                mcSound.sfxPlayer.clip = mcSound.mcSfx[0];
-                mcSound.sfxPlayer.pitch = Random.Range(mcSound.lowPitchRange, mcSound.highPitchRange);          //Testa om detta funkar istället för velocityskiten
-                mcSound.sfxPlayer.volume = Random.Range(mcSound.lowPitchRange, mcSound.highPitchRange);
-
             }
-
-            if (controller.velocity.magnitude > 2 && mcSound.sfxPlayer.isPlaying == false)
-            {
-                mcSound.sfxPlayer.clip = mcSound.mcSfx[0];
-                 mcSound.sfxPlayer.pitch = Random.Range(mcSound.lowPitchRange, mcSound.highPitchRange);
-                 mcSound.sfxPlayer.volume = Random.Range(mcSound.lowPitchRange, mcSound.highPitchRange);
-
-                mcSound.sfxPlayer.PlayDelayed(0.37f);      
-                
-              
-            }
-            
-           
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
-
     }
 
     //Så spelaren kan flytta object genom att putta dem.
@@ -205,8 +170,6 @@ public class PlayerManager : MonoBehaviour
 
         Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
         body.velocity = pushDirection * pushForce;
-
-
     }
 
     void OnTriggerEnter(Collider other) //När spelaren går in i collidern
@@ -215,21 +178,22 @@ public class PlayerManager : MonoBehaviour
         if (other.tag == "Climbable")
         {
             canClimb = true;
-
         }
+
         if (other.tag == "Draggable")
         {
             canDrag = true;
             currentDragable = other.gameObject;
         }
+
         if (other.tag == "Safezone")
         {
             if (other.gameObject.GetComponent<ItemManager>().StationHealth > 0)
             {
                 currentStation = other.gameObject;
             }
-
         }
+
         if (other.tag == "Death Zone")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -243,22 +207,24 @@ public class PlayerManager : MonoBehaviour
         {
             canClimb = false;
             moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-
         }
+
         if (other.tag == "Draggable")
         {
             canDrag = false;
             currentDragable = null;
             movementSpeed = 6;
 
+            anim.SetBool("isPulling", false);
         }
+
         if (other.tag == "Safezone")
         {
+            anim.SetBool("isHammering", false);
             currentStation = null;
             isDetectable = true;
         }
     }
-
 
 
 }
